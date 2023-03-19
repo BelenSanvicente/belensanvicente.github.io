2f53f2162b60d2e793b5f28150ccf73af3aaf0f2
BelenSanvicente <94880489+BelenSanvicente@users.noreply.github.com>
2023-03-19 02:53:39 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 72,
+});
+
+
+typewriter
+  .pauseFor(1000)
+  .typeString('Soy estudiante de Física Biomédica, becaria en Pilares CDMX y programadora.')
+  .pauseFor(200)
+  .deleteChars(5)
+  .start();
+
+ 
