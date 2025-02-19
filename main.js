7d65dae4e6e43ebf1ec4e0c652def793f2dc6f6e
BelenSanvicente <94880489+BelenSanvicente@users.noreply.github.com>
2025-02-19 16:07:11 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-let app = document.getElementById('typewriter');
- 
-let typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 72,
-});
 
-
-typewriter
-  .pauseFor(1000)
-  .typeString('Soy estudiante de Física Biomédica y programadora.')
-  .pauseFor(200)
-  .deleteChars(5)
-  .start();
 
  
